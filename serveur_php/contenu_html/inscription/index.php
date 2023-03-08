6b47be15748737155462b1f5c588b92f381dda9d
nicolas745 <47307856+nicolas745@users.noreply.github.com>
2023-03-08 16:12:12 +0100

    corection bug css

--- serveur_php/contenu_html/inscription/index.php
+++ serveur_php/contenu_html/inscription/index.php
@@ -1,35 +1,21 @@
-<?php
-function buttons()
-{
-?>
-    <div>
-        <button class="Suivent"></button>
-        <button class="Suivent"></button>
-        <button class="Suivent"></button>
-        <button class="Suivent"></button>
-    </div>
-<?php
-}
-?>
 <main>
-    <section id="page1">
-        <p>pseudo : </p>
-        <input>
-        <p>mots de passe : </p>
-        <input>
-        <p>Sex :</p>
-        <div>
-            <input type="radio" name="sexe" value="garcon">
-            <?php include("media/garcon.svg"); ?>
-            <input type="radio" name="sexe" value="fille">
-            <?php include("media/fille.svg"); ?>
-            <input name="sexe" value="nobinaire" type="radio">
-            <?php include("media/nonbinaire.svg"); ?>
-        </div>
-        <p>E-mail</p>
-        <input>
-        <button class="Suivent">Suivent</button>
-        <?php buttons(); ?>
-    </section>
+    <?php
+    $dir = 'contenu_html/inscription'; // Mettez le chemin absolu de votre dossier ici
+    $files = scandir($dir);
+    function buttons($nb)
+    {
+        for ($i = 0; $i < $nb; $i++) {
+            echo "<button>$i</button>";
+        }
+    }
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..' && $file !== 'index.php') {
+            echo '<section id="' . $file . '">';
+            include("$dir/$file");
+            buttons(count($files) - 3);
+            echo "</section>";
+        }
+    }
+    ?>
     <script src="js/inscription.js"></script>
 </main>
---
+++ serveur_php/contenu_html/inscription/page1.php
@@ -0,0 +1,14 @@
+<p>pseudo : </p>
+<input>
+<p>mots de passe : </p>
+<input>
+<p>Sex :</p>
+<input type="radio" name="sexe" value="garcon">
+<?php include("media/garcon.svg"); ?>
+<input type="radio" name="sexe" value="fille">
+<?php include("media/fille.svg"); ?>
+<input name="sexe" value="nobinaire" type="radio">
+<?php include("media/nonbinaire.svg"); ?>
+<p>E-mail</p>
+<input>
+<button class="Suivent">Suivent</button>
