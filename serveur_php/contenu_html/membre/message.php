0c11f2f43f25599d11e831029c300b6a48c98a95
nicolas745 <47307856+nicolas745@users.noreply.github.com>
2023-03-17 14:36:41 +0100

    message  post sur le client et send au serveur node

--- serveur_php/contenu_html/membre/message.php
+++ serveur_php/contenu_html/membre/message.php
@@ -1,14 +1,15 @@
 <script>
+    my = <?php echo $_SESSION['id']; ?>;
     key = "<?php
             $chiffrement = new chiffrement();
             echo $chiffrement->encrypt($_SESSION['id'], "16b0fa40cdd34bbf92ae41f7dda24681a7dc2665ea689a009dbd14343709ec00");
             ?>";
 </script>
-<nav>lsl</nav>
-<article>ss</article>
+<h2>lsl</h2>
+<article id="convers">ss</article>
 <form method="post">
     <fieldset>
-        <textarea name="message"></textarea>
+        <textarea name="message" id="text"></textarea>
         <button id="message">
             <?php include(icon::message); ?>
         </button>
--- serveur_php/js/membre.js
+++ serveur_php/js/membre.js
@@ -1,11 +1,32 @@
 function message() {
 
 }
+let action = function () {
+    alert("erreur");
+};
+var params = new URLSearchParams(location.search);
 if (typeof io !== 'undefined') {
     var socket = io('http://localhost:3000');
     socket.on("message", message);
+    action = function () {
+        if (document.getElementById("text").value !== "") {
+            text = document.createElement("p");
+            text.append(document.getElementById("text").value);
+
+            document.getElementById("convers").append(text);
+            socket.emit("message", {
+                "key": key,
+                "id_ami": params.get('id'),
+                "id_my": my,
+                "message": document.getElementById("text").value
+            });
+        }
+    }
 } else {
 
 }
 
-document.getElementById("message");
+document.getElementById("message").addEventListener("click", (e) => {
+    e.preventDefault();
+    action();
+})
